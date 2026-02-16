f3ec599d66cbe6e938f4c13504bd75da7ee44176
Spencer Johnson <sptjohns@ucsc.edu>
2026-02-15 17:18:56 -0800

    switched block from string to array of char with fixed size

--- include/components.hpp
+++ include/components.hpp
@@ -1,17 +1,20 @@
 #pragma once
-#include <string>
+#include <cstring>
 #include <vector>
 
+#define BLOCK_SIZE 4096
 
 struct Block {
     int id;
-    std::string data;
+    char data[BLOCK_SIZE];
     bool is_dummy;
 
     Block()
         : id(-1), data(""), is_dummy(true) {}
 
-    Block(int id_, const std::string& data_) : id(id_), data(data_), is_dummy(false) {}
+    Block(int id_, const char* data_) : id(id_), is_dummy(false) {
+        std::copy(data_, data_ + BLOCK_SIZE, data);
+    }
 };
 
 class Bucket {
@@ -24,7 +27,7 @@ public:
         blocks.resize(Z);
         for (int i = 0; i < Z; ++i) {
             blocks[i].id = -1; // -1 indicates a dummy block
-            blocks[i].data = "";
+            std::memset(blocks[i].data, 0, BLOCK_SIZE); // empty data`
             blocks[i].is_dummy = true;
         }
     }
