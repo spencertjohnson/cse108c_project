e33891ffb2a597405e5e97c623aedabc4324a8d4
Spencer Johnson <sptjohns@ucsc.edu>
2026-03-13 14:03:49 -0700

    changed components to serialize data, prepping to swap every operation to disk instead of memory

--- include/components.hpp
+++ include/components.hpp
@@ -3,36 +3,51 @@
 #include <vector>
 
 #define BLOCK_SIZE 4096
+#define Z 4
+
+static constexpr int DISK_BLOCK_SIZE  = 4 + BLOCK_SIZE;   // id + data
+static constexpr int DISK_BUCKET_SIZE = Z * DISK_BLOCK_SIZE;
 
 struct Block {
     int id;
-    char data[BLOCK_SIZE];
-    bool is_dummy;
+    uint8_t data[BLOCK_SIZE];
     std::vector<int> tags; // rORAM distributed position map (stores bit-reversed positions)
 
     Block()
-        : id(-1), is_dummy(true) {
+        : id(-1) {
             std::memset(data, 0, BLOCK_SIZE);
         }
 
-    Block(int id_, const char* data_) : id(id_), is_dummy(false) {
-        std::strncpy(data, data_, BLOCK_SIZE - 1);
-        data[BLOCK_SIZE - 1] = '\0';
+    Block(int id_, const uint8_t * data_) : id(id_) {
+        std::memcpy(data, data_, BLOCK_SIZE);
+    }
+
+    bool is_dummy() const { return id == -1; }
+
+    void serialize(uint8_t* buf) const {
+        std::memcpy(buf, &id, 4);
+        std::memcpy(buf + 4, data, BLOCK_SIZE);
+    }
+
+    void deserialize(const uint8_t* buf) {
+        std::memcpy(&id, buf, 4);
+        std::memcpy(data, buf + 4, BLOCK_SIZE);
     }
 };
 
 class Bucket {
 public:
-    std::vector<Block> blocks;
+    Block blocks[Z];
 
     Bucket() = default;
 
-    Bucket(int Z) {
-        blocks.resize(Z);
-        for (int i = 0; i < Z; ++i) {
-            blocks[i].id = -1; // -1 indicates a dummy block
-            std::memset(blocks[i].data, 0, BLOCK_SIZE);
-            blocks[i].is_dummy = true;
-        }
+    void serialize(uint8_t* buf) const {
+        for (int i = 0; i < Z; ++i)
+            blocks[i].serialize(buf + i * DISK_BLOCK_SIZE);
+    }
+
+    void deserialize(const uint8_t* buf) {
+        for (int i = 0; i < Z; ++i)
+            blocks[i].deserialize(buf + i * DISK_BLOCK_SIZE);
     }
 };
