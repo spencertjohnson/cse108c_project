0819ea3ecd04f36530b96ebec03d99073241a9dc
Spencer Johnson <sptjohns@ucsc.edu>
2026-03-13 18:49:24 -0700

    updated components

--- include/components.hpp
+++ include/components.hpp
@@ -1,14 +1,13 @@
 #pragma once
 #include <cstring>
-#include <vector>
 
 #define BLOCK_SIZE 4096
 #define Z 4
 
 static constexpr int MAX_TAGS = 16;
 
-// [ id: 4 bytes | data: BLOCK_SIZE bytes | num_tags: 4 bytes | tags: MAX_TAGS*4 bytes ]
-static constexpr int DISK_BLOCK_SIZE  = 4 + BLOCK_SIZE + 4 + MAX_TAGS * 4;
+// [ id: 4 bytes | data: BLOCK_SIZE bytes | tags: MAX_TAGS*4 bytes ]
+static constexpr int DISK_BLOCK_SIZE = 4 + BLOCK_SIZE + MAX_TAGS * 4;
 static constexpr int DISK_BUCKET_SIZE = Z * DISK_BLOCK_SIZE;
 
 struct Block {
@@ -30,7 +29,7 @@ struct Block {
     }
 
     //Read rORAM block
-    Block(int id_, const uint8_t * data_, const int * tags_,) : id(id_) {
+    Block(int id_, const uint8_t * data_, const int * tags_) : id(id_) {
         std::memcpy(data, data_, BLOCK_SIZE);
         std::memset(tags, 0, sizeof(tags));
         std::memcpy(tags, tags_, MAX_TAGS * 4);
@@ -41,7 +40,7 @@ struct Block {
     void serialize(uint8_t* buf) const {
         std::memcpy(buf,                         &id,       4);
         std::memcpy(buf + 4,                      data,     BLOCK_SIZE);
-        std::memcpy(buf + 4 + BLOCK_SIZE + 4,    tags,      MAX_TAGS * 4);
+        std::memcpy(buf + 4 + BLOCK_SIZE, tags, MAX_TAGS * 4);
 
     }
 
