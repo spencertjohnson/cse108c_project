ffcd1a5710e51bfb3bffc742d5f4a57d31120c2c
Spencer Johnson <sptjohns@ucsc.edu>
2026-03-18 19:32:32 -0700

    cleaned up components

--- include/components.hpp
+++ include/components.hpp
@@ -4,35 +4,23 @@
 #define BLOCK_SIZE 4096
 #define Z 4
 
-static constexpr int MAX_TAGS = 16;
-
 // [ id: 4 bytes | data: BLOCK_SIZE bytes | tags: MAX_TAGS*4 bytes ]
-static constexpr int DISK_BLOCK_SIZE = 4 + BLOCK_SIZE + MAX_TAGS * 4;
+static constexpr int DISK_BLOCK_SIZE = 4 + BLOCK_SIZE;
 static constexpr int DISK_BUCKET_SIZE = Z * DISK_BLOCK_SIZE;
 
 struct Block {
     int id;
     uint8_t data[BLOCK_SIZE];
-    int tags[MAX_TAGS];
 
     // Dummy block
     Block()
         : id(-1) {
             std::memset(data, 0, BLOCK_SIZE);
-            std::memset(tags, 0, sizeof(tags));
         }
 
     // Real PATH ORAM block
     Block(int id_, const uint8_t * data_) : id(id_) {
         std::memcpy(data, data_, BLOCK_SIZE);
-        std::memset(tags, 0, sizeof(tags));
-    }
-
-    //Read rORAM block
-    Block(int id_, const uint8_t * data_, const int * tags_) : id(id_) {
-        std::memcpy(data, data_, BLOCK_SIZE);
-        std::memset(tags, 0, sizeof(tags));
-        std::memcpy(tags, tags_, MAX_TAGS * 4);
     }
 
     bool is_dummy() const { return id == -1; }
@@ -40,14 +28,12 @@ struct Block {
     void serialize(uint8_t* buf) const {
         std::memcpy(buf,                         &id,       4);
         std::memcpy(buf + 4,                      data,     BLOCK_SIZE);
-        std::memcpy(buf + 4 + BLOCK_SIZE, tags, MAX_TAGS * 4);
 
     }
 
     void deserialize(const uint8_t* buf) {
         std::memcpy(&id,   buf,                   4);
         std::memcpy( data, buf + 4,               BLOCK_SIZE);
-        std::memcpy( tags, buf + 4 + BLOCK_SIZE,  MAX_TAGS * 4);
     }
 };
 
